2cb602b1d81085235452840ee2e06244a512e0e5
Mengyun Liu <amylmy@users.noreply.github.com>
2019-03-24 09:59:31 +0800

    Create 2019_Practice_Round_NumberGuessing.java

---
+++ 2019_Practice_Round_NumberGuessing.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Solution {
+  public static void solve(Scanner input, int a, int b) {
+    int m = (a + b) / 2;
+    System.out.println(m);
+    String s = input.next();
+    if (s.equals("CORRECT")) {
+      return;
+    } else if (s.equals("TOO_SMALL")) {
+      solve(input, m + 1, b);
+    } else {
+      solve(input, a, m - 1);
+    }
+  }
+
+  public static void main(String args[]) {
+    Scanner input = new Scanner(System.in);
+    int T = input.nextInt();
+    for (int ks = 1; ks <= T; ks++) {
+      int a = input.nextInt();
+      int b = input.nextInt();
+      int n = input.nextInt();
+      solve(input, a + 1, b);
+    }
+  }
+}
