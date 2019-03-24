98625ceee7c4546401bc530064f64cbdb3f9dd47
Mengyun Liu <amylmy@users.noreply.github.com>
2019-03-24 09:54:36 +0800

    Update 2019_Practice_Round_Mural.java

--- 2019_Practice_Round_Mural.java
+++ 2019_Practice_Round_Mural.java
@@ -24,6 +24,7 @@ public class Solution {
         int caseNum = input.nextInt();
         for (int ks = 1; ks <= caseNum; ks++) {
             System.out.println("Case #" + ks + ": " + solve(input));
+            System.out.println(String.format("Case #%d: %s", ks, solve(input)));
         }
     }
 }
